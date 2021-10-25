2ab4fd487bce1085acf392bd1371bf8029ef6f59
sinhnguyen <sinhnt990@gmail.com>
2021-10-25 17:13:43 +0700

    ok

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,14 +5,14 @@ exports.authentication = (req, res, next) => {
     const token = req.header('token')
     console.log(token);
     if (!token) {
-        return res.status(401).json({message: 'Chua cap quyen truy cap'})
+        return res.status(401).json({ message: 'Chua cap quyen truy cap' })
     }
     jwt.verify(token, 'fptHCM', (err, user) => {
         if (err) {
-            return res.status(500).json({message: 'ban ko co quyen truy cap'})
+            return res.status(500).json({ message: 'ban ko co quyen truy cap' })
         }
         req.user = user
-        console.log('user verify'+ user);
+        console.log('user verify' + user);
         next()
     })
 }
